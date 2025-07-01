ea3720ce52a64525b9ba7270cf1e4ca46385af22
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-07-02 00:22:15 +0200

    Improved use of calendar API

--- js/kalender.js
+++ js/kalender.js
@@ -1,14 +1,33 @@
 window.addEventListener('load', initCalendar);
 
-async function initCalendar() {
-  const events = await fetchCalendar();
-  console.table(events);
+const eventCache = {};
 
+async function initCalendar() {
   const calendarEl = document.getElementById('calendar');
 
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
-    events: events.map(({ title, start, end }) => ({ title, start, end })),
+    events: async (fetchInfo, successCallback, failureCallback) => {
+      try {
+        const start = fetchInfo.start;
+        const end = fetchInfo.end;
+        const requests = [];
+        const currentDate = new Date(start.getFullYear(), start.getMonth(), 1);
+
+        while (currentDate < end) {
+          const year = currentDate.getFullYear();
+          const month = currentDate.getMonth() + 1;
+          requests.push(fetchCalendar(month, year));
+          currentDate.setMonth(currentDate.getMonth() + 1);
+        }
+
+        const eventArrays = await Promise.all(requests);
+        const events = [].concat(...eventArrays);
+        successCallback(events);
+      } catch (error) {
+        failureCallback(error);
+      }
+    },
   });
 
   calendar.setOption('locale', 'sv');
@@ -16,12 +35,22 @@ async function initCalendar() {
   calendar.render();
 }
 
-async function fetchCalendar() {
-  const d = new Date();
-  const month = d.getMonth() + 1;
-  const year = d.getFullYear();
+async function fetchCalendar(month, year) {
+  const cacheKey = `${year}-${month}`;
+  if (eventCache[cacheKey]) {
+    return eventCache[cacheKey];
+  }
 
-  return fetch(
+  const d = new Date();
+  month = month || d.getMonth() + 1;
+  year = year || d.getFullYear();
+
+  // `https://ifk-hindas.netlify.app/.netlify/functions/calendar?month=${month}&year=${year}`
+  console.log(`Fetching events for month: ${month}, year: ${year}`);
+  const events = await fetch(
     `https://ifk-hindas.netlify.app/.netlify/functions/calendar?month=${month}&year=${year}`
   ).then((res) => res.json());
+
+  eventCache[cacheKey] = events;
+  return events;
 }
